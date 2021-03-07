dadbe6bbcecdc06996a76a1b30242d23d74b3016
Paul Davies <lagdotcom@gmail.com>
2021-03-07 18:53:45 +0000

    initial commit; it's a repo

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import sourcemaps from "rollup-plugin-sourcemaps";
+import typescript from "rollup-plugin-typescript2";
+import url from "@rollup/plugin-url";
+
+export default {
+  input: pkg.main,
+  output: {
+    sourcemap: true,
+    file: "dist/bundle.js",
+    format: "iife",
+  },
+  plugins: [
+    url({ limit: Infinity }),
+    sourcemaps(),
+    commonjs(),
+    resolve(),
+    typescript({ tsconfigDefaults: { sourceMap: true } }),
+    serve({ contentBase: "dist", open: true }),
+  ],
+};
