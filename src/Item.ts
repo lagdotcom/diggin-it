e5414a242d39fc548200877c5ef6f64648c67a04
Paul Davies <lagdotcom@gmail.com>
2021-03-08 20:45:53 +0000

    items can now fall (this is stupid)

--- src/Item.ts
+++ src/Item.ts
@@ -1,17 +1,21 @@
 export interface ItemOptions {
   article: string;
+  canClimb: boolean;
   colour: string;
   glyph: string;
   name: string;
+  obeysGravity: boolean;
   treasure: number;
 }
 
 export default class Item {
   type: "item";
   article: string;
+  canClimb: boolean;
   colour: string;
   glyph: string;
   name: string;
+  obeysGravity: boolean;
   treasure: number;
 
   constructor(
@@ -19,17 +23,21 @@ export default class Item {
     public y: number,
     {
       article = "a",
+      canClimb = false,
       colour = "silver",
       glyph = "?",
       name = glyph,
+      obeysGravity = true,
       treasure = 0,
     }: Partial<ItemOptions> = {}
   ) {
     this.type = "item";
     this.article = article;
+    this.canClimb = canClimb;
     this.colour = colour;
     this.glyph = glyph;
     this.name = name;
+    this.obeysGravity = obeysGravity;
     this.treasure = treasure;
   }
 }
--- src/systems/Gravity.ts
+++ src/systems/Gravity.ts
@@ -1,5 +1,5 @@
-import Actor from "../Actor";
 import Game from "../Game";
+import Thing from "../interfaces/Thing";
 
 export default class Gravity {
   constructor(public g: Game) {}
@@ -9,33 +9,42 @@ export default class Gravity {
   }
 
   runOnce(): boolean {
-    const { actors, map } = this.g;
+    const { actors, items, map } = this.g;
     var affected = 0;
 
     for (let y = map.height - 1; y >= 0; y--) {
       for (let x = 0; x < map.width; x++) {
         const victim = actors.get(x, y);
-        if (!victim) continue;
-        if (!victim.obeysGravity) continue;
+        if (this.check(victim)) affected++;
 
-        const current = this.g.map.get(x, y);
-        if (current.canSwimIn) continue;
-
-        const { actor, items, tile } = this.g.contents(x, y + 1);
-
-        // TODO: can things sit on other things?
-        if (actor) continue;
-        if (tile.solid || (victim.canClimb && tile.canStandOn)) continue;
-
-        // we got a match!
-        if (this.fall(victim)) affected++;
+        const present = items.get(x, y);
+        present.forEach((item) => {
+          if (this.check(item)) affected++;
+        });
       }
     }
 
     return affected > 0;
   }
 
-  fall(thing: Actor): boolean {
+  check(victim: Thing) {
+    if (!victim) return;
+    if (!victim.obeysGravity) return;
+
+    const current = this.g.map.get(victim.x, victim.y);
+    if (current.canSwimIn) return;
+
+    const { actor, items, tile } = this.g.contents(victim.x, victim.y + 1);
+
+    // TODO: can things sit on other things?
+    if (actor && victim.type === "actor") return;
+    if (tile.solid || (victim.canClimb && tile.canStandOn)) return;
+
+    // we got a match!
+    return this.fall(victim);
+  }
+
+  fall(thing: Thing): boolean {
     var { x, y } = thing;
     var contents = this.g.contents(x, y + 1);
     var distance = 0;
@@ -47,7 +56,11 @@ export default class Gravity {
       if (actor) break;
 
       // landed
-      if (tile.solid || (thing.canClimb && tile.canStandOn)) break;
+      if (
+        tile.solid ||
+        (thing.type === "actor" && thing.canClimb && tile.canStandOn)
+      )
+        break;
 
       y++;
       distance++;
